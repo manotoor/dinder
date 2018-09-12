00ed7bae2175eeb3ca5308389a8b0f08d97dfdd8
Mano Toor <manotoor@gmail.com>
2018-09-11 17:25:03 -0700

    Working matches page issue with firebase and getting posts

--- app/components/match.component.js
+++ app/components/match.component.js
@@ -7,7 +7,7 @@ import {
 	CardItem,
 	DeckSwiper
 } from 'native-base';
-import { View, Text, Image} from 'react-native';
+import { StyleSheet, View, Text, Image} from 'react-native';
 import { observer } from 'mobx-react/native';
 import { observable } from 'mobx';
 import { createAutoSubscriber, autoSubscriber} from 'firebase-nest';
@@ -42,10 +42,10 @@ class Match extends Component {
 			return (
 				<Card>
 					<CardItem cardBody>
-						{ pic.uri != undefined && pic.uri != "" ? <Image source={pic}/> : null}
+						{ pic.uri != undefined && pic.uri != "" ? <Image style={styles.thumbnail} source={pic}/> : null}
 					</CardItem>
 					<CardItem>
-						<Text>
+						<Text style={styles.text}>
 							{text}
 						</Text>
 					</CardItem>
@@ -59,14 +59,53 @@ class Match extends Component {
 		return(
 			<Card>
 				<CardItem cardBody>
-					<Text> Out of Matches </Text>
+					<Text style={styles.text}> Out of Matches </Text>
 				</CardItem>
 			</Card>
 		)
 	}
 	render(){
-		return null;
+		const {matches, auth} = this.props.stores;
+		const user = auth.authUser;
+
+		const {_autoSubscriberFetching: fetching, _autoSubscriberError: fetchError} = this.state;
+
+		if(fetchError){
+			return <Text style={{backgroundColor: "red"}}>
+						{fetchError}
+					</Text>
+		}
+		const postList = matches.getData('matches')
+		const list = postList ? postList.entries() : null
+		return (
+			<View>
+				{fetching ? <Spinner/> : 
+					<DeckSwiper 
+						dataSource={list} 
+						renderItem={(card) => this.renderCard(card, matches)}
+						renderEmpty={this.renderNoMoreCards.bind(this)}
+						looping = {false}
+						onSwipeRight={(item) => this.markViewed(item)}
+						onSwipeLeft={(item)=> this.markViewed(item)}
+					/>
+				}
+			</View>
+		)
 	}
 }
+
+const styles = StyleSheet.create({
+	text: {
+		color: 'black',
+		fontSize: 20,
+		paddingTop: 10,
+		paddingBottom: 10
+	},
+	thumbnail:{
+		width: 300,
+		height: 300,
+		flex: 1
+	}
+})
 
 export default autoSubscriber(observer(Match))
